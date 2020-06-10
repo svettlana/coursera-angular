9d53c8a8f9afbde161b4db587ca74c8c73a415cd
svettlana <64417964+svettlana@users.noreply.github.com>
2020-06-10 14:14:26 +0300

    Update app.js

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -1,29 +1,51 @@
-(function () {
-'use strict';
+(function(){
+	'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = ['$scope'];
+	angular.module("LunchCheck",[])
+	.controller("LunchCheckController",lunchCheckController);
+	lunchCheckController.$inject=['$scope'];
 
- function LunchCheckController($scope) {
-   $scope.list_menu = "input list";
-   $scope.CheckIfTooMuch = function () {
-    const words = $scope.list_menu.split(',');
-    var counter=words.length-1;
-  if (counter === 0) {
-      return 'Please enter data first';
-    }
-    if (counter<3) {
-      return "Enjoy!";
-    }
-    else{
-      return counter + " Is Too much!";
-    }
 
-  };
- $scope.Clear = function () {
-    $scope.list_menu="";
- };
-}
-})();
+	function lunchCheckController($scope){
+		$scope.lunchList="";
+		$scope.message="";
+
+
+		$scope.checkDishes=function(){
+			var cnt=checkIfTooMuch();
+			$scope.message=showMessage(cnt);
+		};
+
+
+		function checkIfTooMuch(){
+			var lunchList=$scope.lunchList;
+			if(lunchList.length>0){
+				var count=0;
+				var lunchMenu=lunchList.split(",");
+				var lunchMenuSize=lunchMenu.length;
+				for(var i=0;i<lunchMenuSize;i++){
+					if(lunchMenu[i].trim().length>0){
+						count++;
+					}
+				}
+				return count;
+			}
+			else{
+				return 0;
+			}
+		}
+
+		function showMessage(cnt){
+			if (cnt==0){
+				return "Please enter data first";
+			}
+			else if(cnt<=3){
+					return "Enjoy!";
+				}
+			else{
+					return "Too Much!";
+				}
+		}
+	}
+	})();
